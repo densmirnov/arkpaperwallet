4391b313af2f4da748969de30f05746b4c8f9eb6
Denis Smirnov <densmirnov@me.com>
2016-12-13 03:13:44 +0300

    Branded for Ark, changed version to 1.0.0

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,3 @@
-
 import gulp from 'gulp'
 import babel from 'gulp-babel'
 import less from 'gulp-less'
@@ -7,34 +6,34 @@ import gulpBrowser from 'gulp-browser'
 import jade from 'gulp-jade'
 
 gulp.task('babel', () => {
-  gulp
-    .src(['src/**/*.js', '!src/Arkwallet.js'])
-    .pipe(watch('src/**/*.js', { verbose: true }))
-    .pipe(babel({ presets: ['es2015'] }))
-    .pipe(gulp.dest('lib'))
+    gulp
+        .src(['src/**/*.js', '!src/wallet.js'])
+        .pipe(watch('src/**/*.js', { verbose: true }))
+        .pipe(babel({ presets: ['es2015'] }))
+        .pipe(gulp.dest('lib'))
 })
 
 gulp.task('less', () => {
-  gulp
-    .src('src/**/*.less')
-    .pipe(watch('src/**/*.less'))
-    .pipe(less())
-    .pipe(gulp.dest('lib'))
+    gulp
+        .src('src/**/*.less')
+        .pipe(watch('src/**/*.less'))
+        .pipe(less())
+        .pipe(gulp.dest('lib'))
 })
 
 gulp.task('jade', () => {
-  gulp
-    .src('src/**/*.jade')
-    .pipe(watch('src/**/*.jade'))
-    .pipe(jade({ pretty: true }))
-    .pipe(gulp.dest('.'))
+    gulp
+        .src('src/**/*.jade')
+        .pipe(watch('src/**/*.jade'))
+        .pipe(jade({ pretty: true }))
+        .pipe(gulp.dest('.'))
 })
 
 gulp.task('bundle', () => {
-  gulp
-    .src('src/Arkwallet.js')
-    .pipe(watch('src/Arkwallet.js', { verbose: true }))
-    .pipe(babel({ presets: ['es2015'] }))
-    .pipe(gulpBrowser.browserify())
-    .pipe(gulp.dest('lib'))
+    gulp
+        .src('src/wallet.js')
+        .pipe(watch('src/wallet.js', { verbose: true }))
+        .pipe(babel({ presets: ['es2015'] }))
+        .pipe(gulpBrowser.browserify())
+        .pipe(gulp.dest('lib'))
 })
